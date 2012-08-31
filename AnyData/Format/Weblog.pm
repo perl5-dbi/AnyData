8ed070854ee0d5367314483dd4befba8c246ab59
Sven Dowideit <SvenDowideit@fosiki.com>
2012-08-31 15:40:42 +1000

    weblog request and referer regexs were too greedy, and the referer and client regex's where in the wrong order (RT#34063) and (RT#72334) Wes Brown <wes [...] smellycat.com> and pawal [...] blipp.co

--- AnyData/Format/Weblog.pm
+++ AnyData/Format/Weblog.pm
@@ -61,12 +61,11 @@ all rights reserved
 
 =cut
 
-
 use strict;
 use AnyData::Format::Base;
 use vars qw( @ISA $DEBUG $VERSION);
 @AnyData::Format::Weblog::ISA = qw( AnyData::Format::Base );
-$DEBUG = 0;
+$DEBUG                        = 0;
 
 $VERSION = '0.06';
 
@@ -74,9 +73,9 @@ sub new {
     my $class = shift;
     my $self = shift || {};
     $self->{col_names} =
-        'remotehost,username,authuser,date,request,status,bytes,client,referer';
-    $self->{record_sep} = "\n";
-    $self->{key} = 'datestamp';
+      'remotehost,username,authuser,date,request,status,bytes,referer,client';
+    $self->{record_sep}      = "\n";
+    $self->{key}             = 'datestamp';
     $self->{keep_first_line} = 1;
     return bless $self, $class;
 }
@@ -84,21 +83,20 @@ sub new {
 sub read_fields {
     print "PARSE RECORD\n" if $DEBUG;
     my $self = shift;
-    my $str  = shift || return undef;
+    my $str = shift || return undef;
     $str =~ s/^\s+//;
     $str =~ s/\s+$//;
     return undef unless $str;
-    my(@row) = $str =~
-        /^(\S*) (\S*) (\S*) \[([^\]]*)\] "(.*)" (\S*) (\S*)\s*(.*)$/;
+    my (@row) =
+      $str =~ /^(\S*) (\S*) (\S*) \[([^\]]*)\] "(.*?)" (\S*) (\S*)\s*(.*)$/;
     return undef unless defined $row[0];
-    my($client,$referer) = $row[7] =~ /^(.*) (\S*)$/;
+    my ( $referer, $client ) = $row[7] =~ /^(.*?)\s(.*)$/;
     $client  ||= '';
     $referer ||= '';
-    ($row[7],$row[8])=($client,$referer);
+    ( $row[7], $row[8] ) = ( $referer, $client );
+
     # $row[3] =~ s/\s*-\s*(\S*)$//; # hide GMT offset on datestamp
-    return @row
+    return @row;
 }
 1;
-
-
 
